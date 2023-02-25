5a8f1b445f607b44ba63798803f88fd86e300331
MwangiKariuki586 <mwangikariuki586.com>
2023-02-25 10:28:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const data = [
+    {
+        id:1,
+        question:"Which of the following programming languages is used on frontend? ",
+        answers: [
+            {answer:"javascript", isCorrect:true},
+            {answer:"python",isCorrect:false},
+            {answer:"ruby", isCorrect:false},
+            {answer:"php", isCorrect:false},
+        ],
+    },
+    {
+        id:2,
+        question:"Which of the following programming languages is used on backend? ",
+        answers: [
+            {answer:"javascript", isCorrect:false},
+            {answer:"python",isCorrect:true},
+            {answer:"typescript", isCorrect:false},
+            {answer:"SaSS", isCorrect:false},
+        ],
+    },
+    {
+        id:3,
+        question:"Which engine does chrome browser run on?",
+        answers: [
+            {answer:"v8 ", isCorrect:true},
+            {answer:"v7",isCorrect:false},
+            {answer:"v6", isCorrect:false},
+            {answer:"v5", isCorrect:false},
+        ],
+    },
+]
+const gameScreen = document.querySelector(".game")
+const resultScreen = document.querySelector(".result")
+const question = document.querySelector(".question")
+const answersContainer = document.querySelector(".answers")
+const submit = document.querySelector(".submit")
+const play = document.querySelector(".play")
+let qIndex = 0
+let correctCount = 0
+let wrongCount = 0
+let total = 0
+let selectedAnswer;
+const playAgain = ()=>{
+    qIndex = 0
+    correctCount= 0
+    wrongCount=0
+    total =0
+    showQuestion(qIndex)
+}
+play.addEventListener("click",()=>{
+    resultScreen.style.display ="none"
+    gameScreen.style.display="block"
+    playAgain()
+})
+const showResult = ()=>{
+    resultScreen.style.display="block"
+    gameScreen.style.display="none"
+    resultScreen.querySelector(".correct").textContent =
+    `Correct Answers:${correctCount}`
+    resultScreen.querySelector(".wrong").textContent =
+    `Wrong Answers:${wrongCount}`
+    resultScreen.querySelector(".score").textContent =
+    `Score:${(correctCount-wrongCount) *10}`
+ }
+ const showQuestion = (qNumber) =>{
+    if(qIndex === data.length) return showResult()
+    selectedAnswer = null
+    question.textContent = data[qNumber].question
+    answersContainer.innerHTML = data[qNumber].answers.map((item,index)=>
+    ` <div class="answer">
+        <input name="answer" type="radio" id=${index} value=${item.isCorrect}>
+        <label for="answer4">${item.answer}</label>
+     </div>
+    `
+    ).join("")
+    selectAnswer()
+ }
+
+ const selectAnswer = ()=>{
+    answersContainer.querySelectorAll("input").forEach((el)=>{
+        el.addEventListener("click", (e)=>{
+            selectedAnswer = e.target.value
+        })
+    })
+ }
+ const submitAnswer = ()=>{
+    submit.addEventListener("click",()=>{
+        if(selectedAnswer!==null){
+            selectedAnswer === "true" ? correctCount++:wrongCount++
+            qIndex++
+            showQuestion(qIndex)
+        }else alert("please answer this question before proceeding to the next one")
+    })
+ }
+
+ showQuestion(qIndex)
+ submitAnswer()
